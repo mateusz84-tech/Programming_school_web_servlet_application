a826a6113aaf9db53396d628813bb0c03165b43c
Mateusz Kocierz <mateusz_kocierz@wp.pl>
2021-05-14 15:25:18 +0200

    Created Servlet to display all group.

---
+++ src/main/java/pl/matkoc/controllers/group/ListGroupServlet.java
@@ -0,0 +1,30 @@
+package pl.matkoc.controllers.group;
+
+import pl.matkoc.dao.GroupDao;
+import pl.matkoc.model.Group;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "ListGroupServlet", value = "/")
+public class ListGroupServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request,
+                         HttpServletResponse response) throws ServletException, IOException {
+
+        GroupDao groupDao = new GroupDao();
+        List<Group> groupList = groupDao.findAll();
+        request.setAttribute("group", groupList);
+        getServletContext().getRequestDispatcher("/group.jsp")
+                .forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request,
+                          HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
