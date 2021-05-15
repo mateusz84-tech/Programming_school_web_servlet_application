a0b36fd7e1c107ae64c4193addbfac30ca991851
Mateusz Kocierz <mateusz_kocierz@wp.pl>
2021-05-15 15:36:57 +0200

    Created servlet to display list exercises and redirect all list to page exercise.

---
+++ src/main/java/pl/matkoc/controllers/exercise/ListExerciseServlet.java
@@ -0,0 +1,33 @@
+package pl.matkoc.controllers.exercise;
+
+import pl.matkoc.dao.ExerciseDao;
+import pl.matkoc.model.Exercise;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "ListExerciseServlet", value = "/exerciseList")
+public class ListExerciseServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request,
+                         HttpServletResponse response) throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        response.setCharacterEncoding("utf-8");
+
+        ExerciseDao exerciseDao = new ExerciseDao();
+        List<Exercise> exerciseList = exerciseDao.findAll();
+        request.setAttribute("list", exerciseList);
+        getServletContext().getRequestDispatcher("/exercise.jsp")
+                .forward(request,response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request,
+                          HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
