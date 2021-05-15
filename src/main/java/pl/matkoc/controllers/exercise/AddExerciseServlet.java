d0e7c574e1d0c92e92c0da9304ec22df2a1e921a
Mateusz Kocierz <mateusz_kocierz@wp.pl>
2021-05-15 15:35:44 +0200

    Created servlet to add new exercise and redirect to next servlet display list.

---
+++ src/main/java/pl/matkoc/controllers/exercise/AddExerciseServlet.java
@@ -0,0 +1,36 @@
+package pl.matkoc.controllers.exercise;
+
+import pl.matkoc.dao.ExerciseDao;
+import pl.matkoc.model.Exercise;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "AddExerciseServlet", value = "/addExercise")
+public class AddExerciseServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request,
+                         HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request,
+                          HttpServletResponse response) throws ServletException, IOException {
+
+        String title = request.getParameter("title");
+        String description = request.getParameter("description");
+
+        Exercise exercise = new Exercise();
+        exercise.setTitle(title);
+        exercise.setDescription(description);
+
+        ExerciseDao exerciseDao = new ExerciseDao();
+        exerciseDao.create(exercise);
+
+        response.sendRedirect("/exerciseList");
+
+    }
+}
